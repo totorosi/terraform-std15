16568ef468c9ba3ee6abf98bc098d4431d2422f8
totorosi <totorosi@users.noreply.github.com>
2026-09-07 14:47:12 +0900

    Fix public network ACL rules

--- ex-net/main.tf
+++ ex-net/main.tf
@@ -227,7 +227,7 @@ resource "aws_network_acl" "public" {
     action     = "allow"
     cidr_block = "0.0.0.0/0"
     from_port  = 0
-    to_port    = 65535
+    to_port    = 0
   }
 
   tags = merge(var.tags, {
